b45bf78446066e6c9b4db30b6db9e58fe8ec368f
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-11 19:19:48 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,49 @@
-function App() {
-  return <div>Example</div>;
-}
+import React, { Component } from 'react';
+import axios from 'axios';
 
-export default App;
+axios.defaults.baseURL = 'https://hn.algolia.com/api/v1';
+
+const ArticleList = ({ articles }) => (
+  <ul>
+    {articles.map(({ objectID, url, title }) => (
+      <li key={objectID}>
+        <a href={url} target="_blank" rel="noreferrer noopener">
+          {title}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
+
+export class App extends Component {
+  state = {
+    articles: [],
+    isLoading: false,
+    error: null,
+  };
+
+  async componentDidMount() {
+    this.setState({ isLoading: true });
+
+    try {
+      const response = await axios.get('/search?query=react');
+      this.setState({ articles: response.data.hits });
+    } catch (error) {
+      this.setState({ error });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
+
+  render() {
+    const { articles, isLoading, error } = this.state;
+
+    return (
+      <div>
+        {error && <p>Whoops, something went wrong: {error.message}</p>}
+        {isLoading && <p>Loading...</p>}
+        {articles.length > 0 && <ArticleList articles={articles} />}
+      </div>
+    );
+  }
+}
